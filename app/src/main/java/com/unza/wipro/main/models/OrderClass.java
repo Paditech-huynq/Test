430ec8bd4c5970f7dcac3bde2759160e66741f8a
Paditech-huynq <huynq@paditech.org>
2018-01-12 16:45:50 +0700

    add test_data

---
+++ app/src/main/java/com/unza/wipro/main/models/OrderClass.java
@@ -0,0 +1,63 @@
+package com.unza.wipro.main.models;
+
+import java.util.Date;
+
+/**
+ * Created by bangindong on 1/12/2018.
+ */
+
+public class OrderClass {
+    private int img;
+    private String title;
+    private Date date;
+    private int price;
+    private int numberOrder;
+
+    public OrderClass(int img, String title, Date date, int price, int numberOrder) {
+        this.img = img;
+        this.title = title;
+        this.date = date;
+        this.price = price;
+        this.numberOrder = numberOrder;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNumberOrder() {
+        return numberOrder;
+    }
+
+    public void setNumberOrder(int numberOrder) {
+        this.numberOrder = numberOrder;
+    }
+}
---
+++ app/src/main/java/com/unza/wipro/main/models/OrderData.java
@@ -0,0 +1,35 @@
+package com.unza.wipro.main.models;
+
+import com.unza.wipro.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by bangindong on 1/12/2018.
+ */
+
+public class OrderData {
+
+    List<OrderClass> list = new ArrayList<>();
+
+    public OrderData() {
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,1,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,2,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,3,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,4,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,5,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,6,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,7,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,10,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2018,12,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2019,1,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2019,2,2,15,25), 1000000, 3));
+        list.add(new OrderClass(R.drawable.background, "mỹ phẩm", new Date(2019,3,2,15,25), 1000000, 3));
+    }
+
+    public List<OrderClass> getList() {
+        return list;
+    }
+}
