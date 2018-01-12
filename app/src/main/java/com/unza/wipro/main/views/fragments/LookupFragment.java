e7735f691caeb19036b9b947de115ed6dd099aaa
kanblack <kanblack.va@gmail.com>
2018-01-12 11:21:32 +0700

    update list lookup

--- app/src/main/java/com/unza/wipro/main/views/fragments/LookupFragment.java
+++ app/src/main/java/com/unza/wipro/main/views/fragments/LookupFragment.java
@@ -1,10 +1,13 @@
 package com.unza.wipro.main.views.fragments;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.paditech.core.BaseFragment;
 import com.unza.wipro.R;
+import com.unza.wipro.main.adapter.LookupAdaper;
+import com.unza.wipro.main.views.customs.VerticalSpacesItemDecoration;
 
 import butterknife.BindView;
 
@@ -21,6 +24,8 @@ public class LookupFragment extends BaseFragment {
     @BindView(R.id.rcvLookup)
     RecyclerView mRecyclerView;
 
+    LookupAdaper mAdaper;
+
     @Override
     protected int getLayoutResource() {
         return R.layout.fragment_home_lookup;
@@ -33,5 +38,18 @@ public class LookupFragment extends BaseFragment {
 
     @Override
     public void setScreenTitle(String title) {
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+        setupRecycleView();
+    }
+
+    private void setupRecycleView() {
+        mAdaper = new LookupAdaper();
+        mRecyclerView.addItemDecoration(new VerticalSpacesItemDecoration(getResources().getDimensionPixelOffset(R.dimen.padding_normal)));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        mRecyclerView.setAdapter(mAdaper);
     }
 }
