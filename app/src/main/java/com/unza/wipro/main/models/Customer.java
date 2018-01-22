fb2893c7b74470b8e8c16f56f6ca9ec64cfc75ae
ToRing <nguyenthetrinh1996@gmail.com>
2018-01-22 11:40:47 +0700

    model and reuse model

--- app/src/main/java/com/unza/wipro/main/models/Customer.java
+++ app/src/main/java/com/unza/wipro/main/models/Customer.java
@@ -5,71 +5,92 @@ import com.google.gson.annotations.SerializedName;
 
 public class Customer {
 
-@SerializedName("id")
-@Expose
-private String id;
-@SerializedName("name")
-@Expose
-private String name;
-@SerializedName("phone")
-@Expose
-private String phone;
-@SerializedName("email")
-@Expose
-private String email;
-@SerializedName("address")
-@Expose
-private String address;
-@SerializedName("avatar")
-@Expose
-private String avatar;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("avatar")
+    @Expose
+    private String avatar;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
 
-public String getId() {
-return id;
-}
+    public String getId() {
+        return id;
+    }
 
-public void setId(String id) {
-this.id = id;
-}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-public String getName() {
-return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public String getPhone() {
-return phone;
-}
+    public String getPhone() {
+        return phone;
+    }
 
-public void setPhone(String phone) {
-this.phone = phone;
-}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-public String getEmail() {
-return email;
-}
+    public String getEmail() {
+        return email;
+    }
 
-public void setEmail(String email) {
-this.email = email;
-}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-public String getAddress() {
-return address;
-}
+    public String getAddress() {
+        return address;
+    }
 
-public void setAddress(String address) {
-this.address = address;
-}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-public String getAvatar() {
-return avatar;
-}
+    public String getAvatar() {
+        return avatar;
+    }
 
-public void setAvatar(String avatar) {
-this.avatar = avatar;
-}
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
