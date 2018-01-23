d02cdc7497bfa5274f854dce2921f1a81707f792
Duc Nguyen <ducnmhn1988@gmail.com>
2018-01-23 11:58:49 +0700

    api 06

---
+++ app/src/main/java/com/unza/wipro/main/presenters/OrderDetailPresenter.java
@@ -0,0 +1,53 @@
+package com.unza.wipro.main.presenters;
+
+import com.paditech.core.mvp.BasePresenter;
+import com.unza.wipro.main.contracts.OrderDetailContract;
+import com.unza.wipro.main.models.LoginClient;
+import com.unza.wipro.main.models.Order;
+import com.unza.wipro.main.models.responses.GetOrderDetailRSP;
+import com.unza.wipro.services.AppClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class OrderDetailPresenter extends BasePresenter<OrderDetailContract.ViewImpl> implements OrderDetailContract.Presenter {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        loadData();
+    }
+
+    @Override
+    public void loadData() {
+        getProductDetail();
+    }
+
+    private void getProductDetail() {
+        Order order = getView().getOrder();
+        if (order == null || !LoginClient.isLogin(getView().getContext())) return;
+        getView().showProgressDialog(true);
+        AppClient.newInstance().getService().getOrderDetail(LoginClient.getToken(getView().getContext()),
+                LoginClient.getAppKey(getView().getContext()), order.getId())
+                .enqueue(new Callback<GetOrderDetailRSP>() {
+                    @Override
+                    public void onResponse(Call<GetOrderDetailRSP> call, Response<GetOrderDetailRSP> response) {
+                        try {
+                            getView().showProgressDialog(false);
+                            if (response.body() != null) {
+                                getView().showOrderDetail(response.body().getOrder());
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<GetOrderDetailRSP> call, Throwable t) {
+                        getView().showProgressDialog(false);
+                        getView().showToast(t.getLocalizedMessage());
+                    }
+                });
+    }
+}
