351a346d9dddd75efec54953f5ba150f147316d3
Paditech-huynq <huynq@paditech.org>
2018-01-24 16:31:45 +0700

    request api to get data

--- app/src/main/java/com/unza/wipro/main/presenters/ProfilePresenter.java
+++ app/src/main/java/com/unza/wipro/main/presenters/ProfilePresenter.java
@@ -1,14 +1,51 @@
 package com.unza.wipro.main.presenters;
 
 import com.paditech.core.mvp.BasePresenter;
+import com.unza.wipro.AppConstans;
 import com.unza.wipro.main.contracts.ProfileContract;
+import com.unza.wipro.main.models.LoginClient;
 import com.unza.wipro.main.models.UserData;
+import com.unza.wipro.main.models.responses.GetOrderDetailRSP;
+import com.unza.wipro.main.models.responses.GetUserProfileRSP;
+import com.unza.wipro.services.AppClient;
+import com.unza.wipro.services.AppService;
+import com.unza.wipro.transaction.user.Customer;
+import com.unza.wipro.transaction.user.Promoter;
+import com.unza.wipro.transaction.user.PromoterLeader;
+import com.unza.wipro.transaction.user.User;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProfilePresenter extends BasePresenter<ProfileContract.ViewImpl> implements ProfileContract.Presenter {
 
     @Override
     public void getUserDataFromServer() {
-        getView().updateUI(UserData.getDummyData());
+        if (AppConstans.app.getCurrentUser() == null ){
+            return;
+        }
+        AppClient.newInstance().getService().getUserProfile(LoginClient.getToken(getView().getContext()),
+                LoginClient.getAppKey(getView().getContext())).enqueue(new Callback<GetUserProfileRSP>() {
+            @Override
+            public void onResponse(Call<GetUserProfileRSP> call, Response<GetUserProfileRSP> response) {
+                getView().updateUI(response.body().getUser());
+                if (AppConstans.app.getCurrentUser() instanceof Customer) {
+                    getView().updateUIForCustomer((Customer) response.body().getUser());
+                }
+                if (AppConstans.app.getCurrentUser() instanceof Promoter) {
+                    getView().updateUIForPromoter((Promoter) response.body().getUser());
+                    if (AppConstans.app.getCurrentUser() instanceof PromoterLeader) {
+                        getView().updateUIForPromoterLeader((PromoterLeader) response.body().getUser());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetUserProfileRSP> call, Throwable t) {
+
+            }
+        });
     }
 
     @Override
